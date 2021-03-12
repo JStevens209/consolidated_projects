28f6a2db82858a85d377e6cff5a92662dcd6f765
Jstevens209 <64735173+JStevens209@users.noreply.github.com>
2021-03-12 14:02:52 -0500

    Started learning golang, moved some project files

---
+++ golang/HelloWorld.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+
+func main() {
+	var thingy  = 1.0
+	const otherThingy = 2
+	thingy = thingy + otherThingy
+	fmt.Println( thingy )
+
+}
+
+
---
+++ golang/PackageTest.go
@@ -0,0 +1,7 @@
+package otherPackage
+
+
+func testThing( thing1, thing2 int ) int {
+
+	return thing1 + thing2
+}
